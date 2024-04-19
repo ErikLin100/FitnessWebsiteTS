3dada732fa4da494329fecb2e4ef39c63079e7f6
ErikLin100 <bgr924@myy.haaga-helia.fi>
2024-04-19 14:14:19 +0300

    Homepage done

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import Navbar from "@/scenes/navbar";
+import Home from "@/scenes/home";
 import { useEffect, useState } from "react";
 import { SelectedPage } from "@/shared/types";
 
@@ -19,14 +20,18 @@ useEffect(() => {
       setIsTopOfPage(true);
       setSelectedPage(SelectedPage.Home);
     }
+    if (window.scrollY !== 0) setIsTopOfPage(false);
   }
+  window.addEventListener("scroll", handleScroll);
+  return () => window.removeEventListener("scroll", handleScroll);
 })
   return <div className="app bg-gray-20">
         <Navbar 
-        
+        isTopOfPage={isTopOfPage}
         selectedPage={selectedPage}
         setSelectedPage={setSelectedPage}
         />
+        <Home setSelectedPage={setSelectedPage} />
     </div>;
   
 }
